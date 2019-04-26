8c8c75211579bac9fc279afb1562aead848c2ee5
chiuy5 <chiuy5@uw.edu>
2019-04-26 09:51:48 -0700

    redid build

---
+++ drifing/build/precache-manifest.ca1974351a431de8c28bbf730c55a824.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82fe8dc63719e20e8ef90ef6f3338a2b",
+    "url": "/drifting/drifing/build/index.html"
+  },
+  {
+    "revision": "d0b89c77c4bc4b0faead",
+    "url": "/drifting/drifing/build/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4fe21f1377af49ba7d0a",
+    "url": "/drifting/drifing/build/static/css/main.095f3b81.chunk.css"
+  },
+  {
+    "revision": "d0b89c77c4bc4b0faead",
+    "url": "/drifting/drifing/build/static/js/2.75d38188.chunk.js"
+  },
+  {
+    "revision": "4fe21f1377af49ba7d0a",
+    "url": "/drifting/drifing/build/static/js/main.c2e88ced.chunk.js"
+  },
+  {
+    "revision": "804bd064212d77ee9994",
+    "url": "/drifting/drifing/build/static/js/runtime~main.4ce0aeb7.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/drifting/drifing/build/static/media/logo.ee7cd8ed.svg"
+  }
+]);
