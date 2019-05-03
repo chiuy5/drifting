2dcc2976b74b2c96c402978e512443ff1e6f0396
chiuy5 <chiuy5@uw.edu>
2019-05-03 10:16:20 -0700

    redid build

--- drifing/build/precache-manifest.71bd71fc12d7358dbf476aaec3da29dc.js
+++ drifing/build/precache-manifest.71bd71fc12d7358dbf476aaec3da29dc.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6584f555c5fd1984d975265e00691aa7",
+    "revision": "c1e210e3af86f4680f478281abd8ad50",
     "url": "/drifting/drifing/build/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/drifting/drifing/build/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "f7c627ab9d78bc46a3a1",
+    "revision": "a8ec4a16979f2a215eb0",
     "url": "/drifting/drifing/build/static/css/main.4889217a.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/drifting/drifing/build/static/js/2.34d7aed8.chunk.js"
   },
   {
-    "revision": "f7c627ab9d78bc46a3a1",
-    "url": "/drifting/drifing/build/static/js/main.afc4a6dd.chunk.js"
+    "revision": "a8ec4a16979f2a215eb0",
+    "url": "/drifting/drifing/build/static/js/main.04758c2a.chunk.js"
   },
   {
     "revision": "804bd064212d77ee9994",
